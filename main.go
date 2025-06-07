ef5a2e06fa42da371e4ef03bb4b05296df4c5e0b
Luis Waldman <luiswaldman@gmail.com>
2025-06-07 10:08:39 -0300

    Agrego Respuesta API

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"github.com/zishang520/socket.io/v2/socket"
 )
 
+func songsHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle the request for songs here
+	// For example, you can return a JSON response with a list of songs
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"songs": ["Song1", "Song2", "Song3"]}`))
+}
+
 func main() {
 	AppConfig, err := ConfigP.LoadConfiguration("config.json")
 	if err != nil {
@@ -18,13 +25,15 @@ func main() {
 	log.Println("Iniciando servidor en puerto", AppConfig.Port)
 	io := socket.NewServer(nil, nil)
 	http.Handle("/socket.io/", io.ServeHandler(nil))
-
 	err = io.On("connection", func(clients ...any) {
 		nuevaConexion(clients)
 	})
 	if err != nil {
 		log.Fatalln("Error setting socket.io on connection", "err", err)
 	}
+	// Handle the new REST endpoint for songs
+	//http.Handle("/socket.io/", io.ServeHandler(nil))
+	http.HandleFunc("/api/songs", songsHandler)
 
 	log.Fatalln(http.ListenAndServe(AppConfig.Port, nil))
 }
