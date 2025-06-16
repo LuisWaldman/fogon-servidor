0ed3acd3f5bff1599ff29743641249436da46043
Luis Waldman <luiswaldman@gmail.com>
2025-06-16 12:40:37 -0300

    Ajustes para produccion

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ import (
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
-		//c.Header("Access-Control-Allow-Origin", "https://www.fogon.ar/")
+		//c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Header("Access-Control-Allow-Origin", "https://www.fogon.ar/")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
