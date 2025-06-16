8ce204de54ba7d1516df47c9ee7ce30efd5a0480
Luis Waldman <luiswaldman@gmail.com>
2025-06-16 15:42:27 -0300

    configuro para desarrollo

--- config/config.go
+++ config/config.go
@@ -7,8 +7,9 @@ type Config struct {
 
 func LoadConfiguration(file string) Config {
 	var config Config = Config{
-		Port:        ":8080",
-		MONGODB_URI: "mongodb+srv://luis:luis@cluster0.n2rothk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+		Port: ":8080",
+		//MONGODB_URI: "mongodb+srv://luis:luis@cluster0.n2rothk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+		MONGODB_URI: "mongodb://localhost:27017",
 	}
 	return config
 }
