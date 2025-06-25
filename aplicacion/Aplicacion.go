8488c282547e0bf66e55763f4ed6cabde9beb03e
Luis Waldman <luiswaldman@gmail.com>
2025-06-24 23:02:00 -0300

    Crea la sesion si no existe y dejo libre el NTP

--- aplicacion/Aplicacion.go
+++ aplicacion/Aplicacion.go
@@ -91,8 +91,8 @@ func (app *Aplicacion) CrearSesion(musico *Musico, sesion string, latitud float6
 func (app *Aplicacion) UnirseSesion(musico *Musico, sesion string) {
 	// Check if the session already exists
 	if _, exists := app.sesiones[sesion]; !exists {
-		musico.Socket.Emit("sesionFailed", "La sesion no existe")
-		return
+		app.CrearSesion(musico, sesion, 0.0, 0.0)
+
 	}
 	musico.UnirseSesion(app.sesiones[sesion])
 }
