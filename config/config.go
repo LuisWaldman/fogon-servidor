9498c0f11bcd4c1b87eaca7ad99e3258ce021ec4
Luis Waldman <luiswaldman@gmail.com>
2025-07-10 00:39:09 -0300

    para prod

--- config/config.go
+++ config/config.go
@@ -8,11 +8,11 @@ type Config struct {
 
 func LoadConfiguration() Config {
 	var config Config = Config{
-		Port: ":8080",
-		//MONGODB_URI: "mongodb+srv://luis:luis@cluster0.n2rothk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-		//Site:        "https://www.fogon.ar",
-		MONGODB_URI: "mongodb://localhost:27017",
-		Site:        "http://localhost:5173",
+		Port:        ":8080",
+		MONGODB_URI: "mongodb+srv://luis:luis@cluster0.n2rothk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+		Site:        "https://www.fogon.ar",
+		//MONGODB_URI: "mongodb://localhost:27017",
+		//Site:        "http://localhost:5173",
 	}
 	return config
 }
