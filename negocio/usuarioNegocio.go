1be4527e110879b355c47f746e643d3e590b5a03
Luis Waldman <luiswaldman@gmail.com>
2025-10-31 20:15:33 -0300

    usuario negocio

---
+++ negocio/usuarioNegocio.go
@@ -0,0 +1,39 @@
+package negocio
+
+import (
+	"github.com/LuisWaldman/fogon-servidor/modelo"
+	"github.com/LuisWaldman/fogon-servidor/servicios"
+)
+
+type UsuarioNegocio struct {
+	usuarioServicio *servicios.UsuarioServicio
+	cancionServicio *servicios.CancionServicio
+	listaServicio   *servicios.ListaServicio
+}
+
+func NuevoUsuarioNegocio(usuarioServicio *servicios.UsuarioServicio, cancionServicio *servicios.CancionServicio, listaServicio *servicios.ListaServicio) *UsuarioNegocio {
+	return &UsuarioNegocio{
+		usuarioServicio: usuarioServicio,
+		cancionServicio: cancionServicio,
+		listaServicio:   listaServicio,
+	}
+}
+
+func (n *UsuarioNegocio) CrearUsuario(nombreUsuario string) error {
+	user := modelo.Usuario{
+		Usuario: nombreUsuario,
+	}
+	return n.usuarioServicio.CrearUsuario(user)
+}
+
+func (n *UsuarioNegocio) BuscarPorNombre(nombreUsuario string) (*modelo.Usuario, error) {
+	return n.usuarioServicio.BuscarPorUsuario(nombreUsuario)
+}
+
+func (n *UsuarioNegocio) GetCancionesPorUsuario(nombreUsuario string) ([]modelo.Cancion, error) {
+	return n.cancionServicio.BuscarPorOwner(nombreUsuario)
+}
+
+func (n *UsuarioNegocio) GetListasPorUsuario(nombreUsuario string) ([]modelo.Lista, error) {
+	return n.listaServicio.BuscarPorOwner(nombreUsuario)
+}
---
+++ negocio/usuarioNegocio_test.go
@@ -0,0 +1,34 @@
+package negocio
+
+import (
+	"testing"
+
+	"github.com/LuisWaldman/fogon-servidor/datos"
+	"github.com/LuisWaldman/fogon-servidor/servicios"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUsuarioNuevo_CrearYBuscar(t *testing.T) {
+	client, err := datos.ConnectDB()
+	assert.Nil(t, err, "Error al conectar a la base de datos: %v", err)
+	nombreUsuario := "NuevoUsuario"
+	usuarioServicio := servicios.NuevoUsuarioServicio(client)
+	cancionServicio := servicios.NuevoCancionServicio(client)
+	listaServicio := servicios.NuevoListaServicio(client)
+	negocioUsuario := NuevoUsuarioNegocio(usuarioServicio, cancionServicio, listaServicio)
+	negocioUsuario.CrearUsuario(nombreUsuario)
+	user, err := negocioUsuario.BuscarPorNombre(nombreUsuario)
+	assert.Nil(t, err, "Error al buscar usuario: %v", err)
+	assert.NotNil(t, user, "Usuario no encontrado")
+	assert.Equal(t, nombreUsuario, user.Usuario, "Nombre de usuario incorrecto")
+	assert.Equal(t, "", user.Clave, "Clave de usuario incorrecta")
+	canciones, err := negocioUsuario.GetCancionesPorUsuario(user.Usuario)
+	assert.Nil(t, err, "Error al obtener canciones: %v", err)
+	assert.Equal(t, 0, len(canciones), "Se esperaban 0 canciones para el usuario")
+	listas, err := negocioUsuario.GetListasPorUsuario(user.Usuario)
+	assert.Nil(t, err, "Error al obtener listas: %v", err)
+	assert.Equal(t, 0, len(listas), "Se esperaban 0 listas para el usuario")
+
+	// Aquí iría la lógica para crear un nuevo usuario
+	// GetCancionesPorUsuario GetListasPorUsuario
+}
